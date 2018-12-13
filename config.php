f7a8dae729fa861d381c3193bf6a979d5c89dc37
yunsxf <45583105+yunsxf@users.noreply.github.com>
2018-12-13 14:35:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4_bur2Nr7Olnc6wjX6yioaulI7cfWO0hXqckaCrl4CHiM6uqnqgv8522q2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=b0ecd7e5e893e71e",
+  ];
+
+?>
